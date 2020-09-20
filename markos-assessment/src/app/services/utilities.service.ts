ff8e077d082c93e19b81879a661bb4f2c87fbbc5
MAIN-PC\Mark <markosagelou@gmail.com>
2020-09-20 11:10:58 +0300

    Cleaned up modifiers for the number generation

--- markos-assessment/src/app/services/utilities.service.ts
+++ markos-assessment/src/app/services/utilities.service.ts
@@ -13,18 +13,18 @@ export class UtilitiesService {
       x: new Date(item.time).getTime(),
       y: item['CPU Usage Millicores'],
     }))
-    .filter((item) => item.y);
+    .filter((item) => item.y !== null);
 
   public instanceData = instanceData
     .map((item) => ({
       x: new Date(item.time).getTime(),
       y: item['Service Instance Count'],
     }))
-    .filter((item) => item.y);
+    .filter((item) => item.y !== null);
 
   public generateValues() {
     const lastValue = this.instanceData[this.instanceData.length - 2].y;
-    const newCPUValue = lastValue + this.randn_bm(-700, 700, -1);
+    const newCPUValue = lastValue + this.rand_normal_dist(-900, 900, -1.4);
     this.instanceData.push({
       x: this.generateNewInstanceTime(),
       y: Math.round(this.cpuData[this.cpuData.length - 2].y / 400),
@@ -49,20 +49,13 @@ export class UtilitiesService {
     return lastTime + 1000 * 30;
   }
 
-  public randn_bm(min, max, skew) {
-    let u = 0;
-    let v = 0;
-    while (u === 0) {
-      u = Math.random();
-    } // Converting [0,1) to (0,1)
-    while (v === 0) {
-      v = Math.random();
-    }
-    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  public rand_normal_dist(min: number, max: number, skew: number) {
+    const u = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u));
 
     num = num / 10.0 + 0.5; // Translate to 0 -> 1
     if (num > 1 || num < 0) {
-      num = this.randn_bm(min, max, skew);
+      num = this.rand_normal_dist(min, max, skew);
     } // resample between 0 and 1 if out of range
     num = Math.pow(num, skew); // Skew
     num *= max - min; // Stretch to fill range
